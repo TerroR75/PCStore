4327d9e10a520270cd81cd99ddff72ab3d7eaebe
TerroR75 <kamilsulimierski@gmail.com>
2023-05-16 14:34:18 +0200

    Added functionality for error testing here

--- client/src/features/about/AboutPage.tsx
+++ client/src/features/about/AboutPage.tsx
@@ -1,7 +1,50 @@
-import { Typography } from '@mui/material';
+import { Alert, AlertTitle, Button, ButtonGroup, Container, List, ListItem, ListItemText, Typography } from '@mui/material';
+import { useState } from 'react';
+import agent from '../../app/api/axiosAgent';
 
 function AboutPage() {
-  return <Typography variant='h2'>AboutPage</Typography>;
+  const [validationErrors, setValidationErrors] = useState<string[]>([]);
+
+  function getValidationError() {
+    agent.TestErrors.getValidationError()
+      .then(() => console.log('should not see this xD'))
+      .catch((error) => setValidationErrors(error));
+  }
+  return (
+    <Container>
+      <Typography variant='h2'>Error Testing</Typography>
+      <ButtonGroup fullWidth>
+        <Button variant='contained' onClick={() => agent.TestErrors.get400Error().catch((error) => console.log(error))}>
+          400 Error
+        </Button>
+        <Button variant='contained' onClick={() => agent.TestErrors.get401Error().catch((error) => console.log(error))}>
+          401 Error
+        </Button>
+        <Button variant='contained' onClick={() => agent.TestErrors.get404Error().catch((error) => console.log(error))}>
+          404 Error
+        </Button>
+        <Button variant='contained' onClick={() => agent.TestErrors.get500Error().catch((error) => console.log(error))}>
+          500 Error
+        </Button>
+        <Button variant='contained' onClick={getValidationError}>
+          Validation error
+        </Button>
+      </ButtonGroup>
+
+      {validationErrors.length > 0 && (
+        <Alert severity='error'>
+          <AlertTitle>Validation Errors</AlertTitle>
+          <List>
+            {validationErrors.map((error) => (
+              <ListItem key={error}>
+                <ListItemText>{error}</ListItemText>
+              </ListItem>
+            ))}
+          </List>
+        </Alert>
+      )}
+    </Container>
+  );
 }
 
 export default AboutPage;
