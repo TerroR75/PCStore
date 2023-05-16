0c02c2c77a21bfe33078c459d84be32392e57779
TerroR75 <kamilsulimierski@gmail.com>
2023-05-16 14:33:25 +0200

    React toastify implementation

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -4,6 +4,8 @@ import { Container, createTheme, CssBaseline } from '@mui/material';
 import { ThemeProvider } from '@emotion/react';
 import { useState } from 'react';
 import { Outlet } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   const [darkMode, setDarkMode] = useState(false);
@@ -23,10 +25,11 @@ function App() {
 
   return (
     <ThemeProvider theme={theme}>
+      <ToastContainer position='bottom-right' hideProgressBar theme='colored' />
       <div className='App'>
         <CssBaseline />
         <Header darkModeChecked={darkMode} checkDarkMode={changeDarkMode} />
-        <Container sx={{ mt: 8 }}>
+        <Container sx={{ mt: 10 }}>
           <Outlet />
         </Container>
       </div>
